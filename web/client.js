c25f7d73ee5385f214f6bf66261afbc5e0bf2bab
Jože Vovk <jozo132@gmail.com>
2019-05-03 19:49:23 +0200

    Created Heat-map function for coloring

--- web/client.js
+++ web/client.js
@@ -2,6 +2,42 @@ var socket;
 
 var visualization = [];
 
+const HEATMAP = (x, domain) => {
+    let extent = domain[1] - domain[0];
+    const getExtent = x => x * extent * 0.20 + domain[0];
+    // @ts-ignore
+    var blue_cyan = d3.scaleLinear()
+        .range(["#0000FF", "#00FFFF"])
+        .domain([getExtent(0), getExtent(1)])
+
+    // @ts-ignore
+    var cyan_green = d3.scaleLinear()
+        .range(["#00FFFF", "#00FF00"])
+        .domain([getExtent(1), getExtent(2)])
+
+    // @ts-ignore
+    var green_yellow = d3.scaleLinear()
+        .range(["#00FF00", "#FFFF00"])
+        .domain([getExtent(2), getExtent(3)])
+
+    // @ts-ignore
+    var yellow_red = d3.scaleLinear()
+        .range(["#FFFF00", "#FF0000"])
+        .domain([getExtent(3), getExtent(4)])
+
+    // @ts-ignore
+    var red_white = d3.scaleLinear()
+        .range(["#FFFF00", "#FFFFFF"])
+        .domain([getExtent(3), getExtent(4)])
+    if (x === null) return "#000000";
+    if (x <= getExtent(1)) return blue_cyan(x);
+    if (x <= getExtent(2)) return cyan_green(x);
+    if (x <= getExtent(3)) return green_yellow(x);
+    if (x <= getExtent(4)) return yellow_red(x);
+    if (x <= getExtent(5)) return red_white(x);
+}
+
+// @ts-ignore
 $(document).ready(() => {
     socket = io();
     socket.on('connect', () => console.log(`Socket connected to server! ${socket.connected}`));
